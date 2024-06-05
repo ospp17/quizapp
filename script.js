659c586cedb38a826f1fdfae1c9097ed77ce6820
octavio scremin <octavio.scremin@eaportal.org>
2024-06-05 13:54:36 +0000

    Adaptações das cores para o tema escuro

---
+++ script.js
@@ -0,0 +1,22 @@
+import{trocarTema,verificarTema} from"./helpers/tema-helper.js"
+
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body,botaoTema)
+})
+
+verificarTema(body,botaoTema)
+
+const botoesAssunto = document.querySelectorAll(".assuntos button")
+botoesAssunto.forEach(botao =>{
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento){
+    const classeBotao = evento.target.className
+    const assunto = document.querySelector(`.${classeBotao} span`).innerText
+   localStorage.setItem("assunto", assunto)
+   window.location.href = "./pages/quiz/quiz.html"
+}
